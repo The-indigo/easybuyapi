7d335dfb722c7171a9d8e13153c0f6cb2506ce79
mudiiiiii <otomudiaga@gmail.com>
2023-04-30 17:10:57 -0400

    initial commit

---
+++ src/test/java/com/example/easybuyapi/EasybuyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easybuyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybuyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
