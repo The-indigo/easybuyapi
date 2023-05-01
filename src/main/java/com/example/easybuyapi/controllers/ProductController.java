4c216a11a1e96382ff5d6d851b68853bd1191234
Abasibiangake James <jamesabasibiangake@gmail.com>
2023-05-01 19:51:10 -0400

    modified test for product category

--- src/main/java/com/example/easybuyapi/controllers/ProductController.java
+++ src/main/java/com/example/easybuyapi/controllers/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,12 +62,14 @@ public class ProductController implements ErrorController {
 		}
 	}
 
-    @GetMapping("/easybuyapi/v1/products")
-    public ResponseEntity<List<ProductModel>> getProductsByCategory(@RequestParam("category") String category)  {
+    @GetMapping("/easybuyapi/v1/products/{category}")
+    public ResponseEntity<List<ProductModel>> getProductsByCategory(@PathVariable String category)  {
         List<ProductModel> products = productRepo.findByCategory(category);
         if(products.isEmpty()) {
+			System.out.println("Not Successfully added");
             return ResponseEntity.noContent().build();
         }
+		System.out.println("Successfully added");
         return ResponseEntity.ok(products);
     }
 
