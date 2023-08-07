1216b9b0e4e291a3344836ac2b8f9a4d91e4ac27
The-indigo <adepojuadeyemi11@gmail.com>
2023-08-07 01:20:45 -0400

    code cleanup

--- src/main/java/com/example/easybuyapi/config/JwtService.java
+++ src/main/java/com/example/easybuyapi/config/JwtService.java
@@ -17,34 +17,84 @@ import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
-    private String SECRET_KEY="6A586E5A7234753778214125442A472D4B6150645367566B5970337335763879";
 
-    public String extractUserId(final String token) {
+    /** sha key.**/
+private String
+hString = "6A586E5A7234753778214125442A472D4B6150645367566B5970337335763879";
+
+/** This method returns the stored values in the token.
+ *
+ * @param token
+ * @return string
+   **/
+    public final String extractUserId(final String token) {
         return extractClaim(
                 token,
                 Claims::getSubject);
     }
 
-    public <T> T extractClaim(final String token, final Function<Claims, T> claimsResolver) {
+    /** The extractClaim method is a utility function for
+     * extracting a specific claim from a JWT using a provided
+     * function. It abstracts away the process of extracting
+     * the claim, making the code more modular and
+     * potentially reusable.
+ *
+ * @param <T>
+ * @param claimsResolver
+ * @param token
+ * @return string
+   **/
+    public final <T> T extractClaim(final String token,
+     final Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    public  String generateToken(final UserDetails userDetails) {
+        /** The generateToken method is an overloaded method
+         * it calls the other generateToken method with an instance of
+         * userDetails as a parameter.
+        *
+        * @param userDetails
+        * @return string
+        **/
+    public final  String generateToken(final UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
-    public String generateToken(final Map<String, Object> extraClaims, final UserDetails userDetails) {
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+
+            /** The generateToken method generates a token
+             * with stored values such as the user details, time issued,
+             * expirationdate
+             * .
+ *
+ * @param extraClaims
+ * @param userDetails
+ * @return string
+   **/
+
+    public final String generateToken(final Map<String,
+     Object> extraClaims, final UserDetails userDetails) {
+        return Jwts.builder().setClaims(
+            extraClaims).setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(
+                System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public boolean isTokenValid(final String token, final UserDetails userDetails) {
+    /** Check the valisity of the token.
+ *
+ * @param token
+ * @param userDetails
+ * @return string
+   **/
+
+    public final boolean isTokenValid(final String token,
+    final UserDetails userDetails) {
         final String username = extractUserId(token);
-        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+        return (username.equals(
+            userDetails.getUsername())) && !isTokenExpired(token);
     }
 
     private boolean isTokenExpired(final String token) {
@@ -56,7 +106,6 @@ public class JwtService {
     }
 
     private Claims extractAllClaims(final String token) {
-        // System.out.println("token is token is"+ token);
         return Jwts.parserBuilder().setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
@@ -64,7 +113,7 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(hString);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
